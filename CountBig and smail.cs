e3b7a3a6c9c64bc2cbe93e4767d7ce3482b7bdd4
Zeds_Xd <125603037+xd-zede32x@users.noreply.github.com>
2023-06-24 19:51:53 +0300

    Add CountBig and smail.cs

---
+++ CountBig and smail.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите первое число: ");
+            int firstNumber = int.Parse(Console.ReadLine());
+            Console.Write("Введите второе число: ");
+            int secondNumber = int.Parse(Console.ReadLine());
+
+            if (firstNumber > secondNumber)
+            {
+                Console.WriteLine("\nПервон число больше второго");
+            }
+
+            else if (firstNumber < secondNumber)
+            {
+                Console.WriteLine("\nВторое число больше первого");
+            }
+
+            else if (firstNumber == secondNumber)
+            {
+                Console.WriteLine("\nОба числа равны друг другу");
+            }
+
+            else
+            {
+                Console.WriteLine("Erorr");
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
